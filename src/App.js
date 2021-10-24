b7fe46e2485eeea2daf569b6c8bbc569bbf8615f
Areeb Khan <areebkhan619@gmail.com>
2021-10-24 20:54:59 +0530

    Added external styling, and animated the list element instead of just the text content

--- src/App.js
+++ src/App.js
@@ -2,46 +2,52 @@ import { useState } from "react";
 import { useTransition, animated } from "react-spring";
 import "./styles.css";
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
       {Array(10)
         .fill(null)
         .map((_, index) => (
-          <span
-            key={index}
-            style={{
-              display: "flex",
-              alignItems: "flex-end",
-              cursor: "pointer"
-            }}
-          >
-            {index + 1}&nbsp;
-            <AnimatedDiv />
-          </span>
+          // <span
+          //   className="list-item"
+          //   key={index}
+          //   style={{
+          //     display: "flex",
+          //     alignItems: "flex-end",
+          //     cursor: "pointer"
+          //   }}
+          // >
+          <AnimatedDiv index={index} />
+          // </span>
         ))}
     </div>
   );
-}
+};
 
-const AnimatedDiv = () => {
+const AnimatedDiv = ({ index }) => {
   const [fadeIn, setFadeIn] = useState(true);
 
   const transition = useTransition(fadeIn, {
     from: { opacity: 0, x: -100, y: -100 },
     enter: { opacity: 1, x: 0, y: 0 },
-    leave: { opacity: 0 }
+    leave: { opacity: 0, x: -100, y: -100 }
   });
 
   return (
     <>
       {transition((style, item) =>
         item ? (
-          <animated.div style={style} onClick={() => setFadeIn(!fadeIn)}>
-            This can get animated{" "}
+          <animated.div
+            className="list-item"
+            style={style}
+            onClick={() => setFadeIn(!fadeIn)}
+          >
+            {index + 1}&nbsp; This can get animated{" "}
           </animated.div>
         ) : null
       )}
     </>
   );
 };
+
+export default App;
